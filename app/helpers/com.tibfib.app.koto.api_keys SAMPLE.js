0cedf3c4f2478fad5efce63121b57a7d75f21e20
Tibfib <Tibfib1@gmail.com>
2011-03-20 20:14:57 -0500

    Added license text and updated readme. Also added sample api keys file

---
+++ app/helpers/com.tibfib.app.koto.api_keys SAMPLE.js
@@ -0,0 +1,11 @@
+api_keys = {
+	lyrics_api_key: "", //api key to musiXmatch.com
+	
+	lastfm_api_key: "", //api key to lastfm
+	lastfm_secret: "", //api secret to lastfm
+	
+	google_api_key: "", //api key for google image search
+	
+	twitter_consumerKey: "", //consumer key for twitter access with xAuth access
+	twitter_consumerSecret: "", //consumer secret for twitter access with xAuth access
+};
